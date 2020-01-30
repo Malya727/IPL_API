b39bbf8e93482bd31042c0174506592479c8d755
Niranjan Malya <niranjanmalya5@gmail.com>
2020-01-30 14:48:28 +0530

    Commit

--- app.py
+++ app.py
@@ -9,37 +9,37 @@ app.json_encoder = MongoJSONEncoder
 def hello():
     return "Hello world"
 
-@app.route("/IPL/Label")
+@app.route("/labels")
 def get_team_label():
     labels  = idb.get_team_labels()
     return jsonify({"labels" : labels})
 
-@app.route("/IPL/Teams")
+@app.route("/teams")
 def get_teams():
     teams = idb.get_all_team_details()
     return jsonify({"teams": teams})
 
-@app.route("/IPL/Teams/<teamname>")
+@app.route("/teams/<teamname>")
 def get_team_by_id(teamname):
     teamMembers = idb.get_team_players(teamname)
     return jsonify({"players" : teamMembers})
 
-@app.route("/IPL/Players/<teamname>")
+@app.route("/players/<teamname>")
 def get_players_num(teamname):
     player_info = idb.get_players_nums(teamname)
     return jsonify({"info" : player_info})
 
-@app.route("/IPL/ALL")
+@app.route("/allcount")
 def get_all_nums():
     nums = idb.get_all_player_nums()
     return jsonify(nums)
 
-@app.route("/IPL/AllStats")
+@app.route("/allstats")
 def get_all_stats():
     nums = idb.get_stats()
     return jsonify({"Stats" : nums})
 
-@app.route("/IPL/AllStats/<teamname>")
+@app.route("/stats/<teamname>")
 def get_all_stats_by_team(teamname):
     nums = idb.get_stats_by_team(teamname)
     return jsonify({"Stats" : nums})
