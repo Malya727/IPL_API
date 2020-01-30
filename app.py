01074d449dbdb4a16c2eee37754f9c3c0b488668
Niranjan Malya <niranjanmalya5@gmail.com>
2020-01-30 15:48:38 +0530

    Final

--- app.py
+++ app.py
@@ -1,58 +1,57 @@
 from flask import Flask,jsonify
-import database as db
+import ipldb as idb
 from mongoflask import MongoJSONEncoder
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)
-
 app.json_encoder = MongoJSONEncoder
+
 @app.route('/')
-def welcome():
-    return jsonify({"message":"Hello World"})
+def index():
+    return jsonify({"message":"Hello We are learning Flask"})
 
-@app.route('/ipl/teamLables')
-def team_names():
-    lables = db.get_team_names()
-    return jsonify({"label":lables})
+@app.route('/ipl/labels')
+def team_labels():
+    labels = idb.get_team_labels()
+    return jsonify({"labels":labels})
 
-@app.route('/ipl/teamDetails')
+@app.route('/ipl/teams')
 def team_details():
-    teams = db.get_team_details()
-    return jsonify({"team":teams})
+    team_details = idb.get_all_team_details()
+    return jsonify({"teams":team_details})
 
 @app.route('/ipl/players')
 def players():
-    players = db.get_all_players()
-    return jsonify({"Player":players})
-
-# ---------------------
+    players = idb.get_all_players()
+    return jsonify({"players":players})
 
 @app.route('/ipl/team/<teamname>')
 def team_players(teamname):
-    players = db.get_team_players(teamname)
+    players = idb.get_team_players(teamname)
+    return jsonify({"players":players})
+@app.route("/ipl/team/rolestat/<teamname>")
+def team_role_count(teamname):
+    team_count = idb.get_players_role_count_by_team(teamname)
+    return jsonify({"stat":team_count})
+
+@app.route("/ipl/team/pricestat/<teamname>")
+def team_income_stats(teamname):
+    stat = idb.get_stat_team(teamname)
+    return jsonify({"stat":stat})
+
+@app.route("/ipl/team/<label>/<teamname>")
+def get_player_by_role_by_team(label,teamname):
+    players = idb.get_players_by_role_by_team(label,teamname)
     return jsonify({"players":players})
 
-@app.route('/ipl/count/<teamname>')
-def players_count(teamname):
-    count = db.get_count_role(teamname)
-    return jsonify({"count":count})
-
-@app.route('/ipl/rolecount')
-def role_count():
-    count = db.get_players_role_count_ipl()
-    return jsonify({"count":count})
-
-@app.route('/ipl/stat/<teamname>')
-def stat(teamname):
-    stat = db.get_stat_team(teamname)
+@app.route("/ipl/team/avgsalipl")
+def all_team_stat():
+    stat = ""
     return jsonify({"stat":stat})
-
-@app.route('/ipl/stat_ipl')
-def stat_ipl():
-    stat = db.get_stat_ipl()
-    return jsonify({"stat":stat})
-
     
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     app.run()
---
+++ ipldb.py
@@ -0,0 +1,75 @@
+from pymongo import MongoClient
+
+uri = "mongodb+srv://db_user:db_user@cluster0-bmww9.mongodb.net/test?retryWrites=true&w=majority"
+
+client = MongoClient(uri)
+db = client.ipl2020
+
+def get_team_labels():
+    collection = db.iplteam
+    labelList = []
+    for x in collection.find({},{"_id":0,"label":1}):
+        labelList.append(x["label"])
+    return labelList
+
+def get_all_team_details():
+    collection = db.iplteam
+    teamList = []
+    for x in collection.find({}):
+        teamList.append(x)
+    return teamList
+
+def get_all_players():
+    collection = db.player
+    players = []
+    for x in collection.find({}):
+        players.append(x)
+    return players
+
+def get_team_players(teamName):
+    collection = db.player
+    players = []
+    for x in collection.find({"label":teamName}):
+        players.append(x)
+    return players
+
+def get_players_role_count_by_team(teamName):
+    collection = db.player
+    x = collection.aggregate([
+        {"$match":{"label":teamName}},
+        {"$group":{"_id":"$role","count":{"$sum":1}}},
+        {"$project":{"role":"$_id","count":1,"_id":0}}
+    ])
+    return [i for i in x]   
+def get_players_role_count_ipl():
+    pass
+# min, max, avg, total salary of the team
+def get_stat_team(teamname):
+    collection = db.player
+    res = collection.aggregate([
+        {"$match":{"label":teamname}},
+        {"$group":
+            {
+                "_id":"$label",
+                "total":{"$sum":"$price"},
+                "max":{"$max":"$price"},
+                "min":{"$min":"$price"},
+                "avg":{"$avg":"$price"}
+            }
+        },
+    ])
+    return [i for i in res]
+# min, max, avg, total salary of the ipl2020
+def get_stat_ipl():
+    collection = db.player
+    res = collection.aggregate([
+        {"$group":{"_id":"$label","total":{"$sum":"$price"},"max":{"$max":"$price"},"min":{"$min":"$price"},"avg":{"$avg":"$price"}}},
+    ])
+    return [i for i in res]
+
+
+def get_players_by_role_by_team(role,teamName):
+    collection = db.player
+    res = collection.find({"role":role,"label":teamName})
+    return [ x for x in res]
+
